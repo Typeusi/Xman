72a8a2a079a03f7f71c2181855eaa59f14c6c21b
ZEZO <nabwalyzydhatm@gmail.com>
2025-05-07 12:29:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Xman/', // هذا مهم للنشر على GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
